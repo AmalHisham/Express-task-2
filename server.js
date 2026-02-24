19a2006daff121f66d436b4a806e0cf000754f53
AmalHisham <115988268+AmalHisham@users.noreply.github.com>
2026-02-24 10:14:04 +0530

    MongoDB Connected

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
 
 app.use(express.json())
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err))
+
+
 app.get("/", (req,res) => {
-    res.json({message : "man!!!, this is working"})
+    res.json({message : "mahn!!!, this is working"})
 })
 
 app.listen(8000)
