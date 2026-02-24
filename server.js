f41434dccb34528434a0cb8fc83115a1370539ec
AmalHisham <115988268+AmalHisham@users.noreply.github.com>
2026-02-24 12:18:41 +0530

    admin login implemented

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 import bycrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import Admin from './models/Admin.js'
 
 const app = express()
@@ -34,6 +35,31 @@ app.post('/register', async (req,res) => {
     await admin.save()
 
     res.json({message : "Admin registered"})
+})
+
+app.post('/login', async (req,res) => {
+
+    const {email,password} = req.body
+
+    const admin = await Admin.findOne({email})
+
+    if(!admin) {
+        return res.status(400).json({message : "Admin not found!!!"})
+    }
+
+    const isMatch = await bycrypt.compare(password, admin.password)
+
+    if(!isMatch) {
+        return res.status(400).json({message : "Password dosen't match"})
+    }
+
+    const token = jwt.sign(
+        {id: admin._id},
+        process.env.JWT_SECRET,
+        {expiresIn  : "1h"}
+    )
+
+    res.json({token})
 })
 
 app.listen(8000)
